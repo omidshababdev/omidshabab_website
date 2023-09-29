8420176c96736fc03892f7a0f11d19ab85430022
Omid Shabab <omidshababdev@gmail.com>
2023-09-29 17:22:25 +0100

    first commit

--- app/[locale]/(routes)/(blog)/layout.tsx
+++ app/[locale]/(routes)/(blog)/layout.tsx
@@ -17,24 +17,36 @@ export default function BlogLayout({
                     <div className="w-full">
                          {children}
                     </div>
-                    <div className="hidden sm:block w-[380px] space-y-[20px]">
-                         <div className="top-0 bg-black bg-opacity-[3%] dark:bg-white dark:bg-opacity-[3%] w-full px-5 py-4 space-y-5">
-                              <div className="space-y-2">
-                                   <p>
-                                        خبرنامه آموزشی
-                                   </p>
-                                   <p className="text-[12px] text-[#3e3e3e] dark:text-[#cbcbcb] leading-[24px]">
-                                        با عضویت در این خبرنامه از جدیدترین پست ها با خبر میشی و هیچ چیزی رو از دست نمیدی!
-                                   </p>
+                    <div className="hidden sm:block w-[380px]">
+                         <div className="top-10 sticky space-y-[20px]">
+                              <div className=" bg-black bg-opacity-[3%] dark:bg-white dark:bg-opacity-[3%] w-full px-5 py-4 space-y-5">
+                                   <div className="space-y-2">
+                                        <p>
+                                             خبرنامه آموزشی
+                                        </p>
+                                        <p className="text-[12px] text-[#3e3e3e] dark:text-[#cbcbcb] leading-[24px]">
+                                             با عضویت در این خبرنامه از جدیدترین پست ها با خبر میشی و هیچ چیزی رو از دست نمیدی!
+                                        </p>
+                                   </div>
+                                   <div className="space-y-2">
+                                        <Input className="bg-transparent rounded-lg dark:border-1 dark:border-white dark:border-opacity-5 focus:border-2 focus:border-opacity-10" placeholder="ایمیل مبارکت رو وارد کن ..." />
+                                        <Button className="w-full rounded-lg" variant="outline">
+                                             می خوام عضو بشم
+                                        </Button>
+                                        <p className="text-[12px] text-[#3e3e3e] dark:text-[#cbcbcb]">
+                                             قول میدم اذیتت نکنم!
+                                        </p>
+                                   </div>
                               </div>
-                              <div className="space-y-2">
-                                   <Input className="bg-transparent rounded-lg dark:border-1 dark:border-white dark:border-opacity-5 focus:border-2 focus:border-opacity-10" placeholder="ایمیل مبارکت رو وارد کن ..." />
-                                   <Button className="w-full rounded-lg" variant="outline">
-                                        می خوام عضو بشم
-                                   </Button>
-                                   <p className="text-[12px] text-[#3e3e3e] dark:text-[#cbcbcb]">
-                                        قول میدم اذیتت نکنم!
-                                   </p>
+                              <div className=" bg-black bg-opacity-[3%] dark:bg-white dark:bg-opacity-[3%] w-full px-6 py-6 space-y-5">
+                                   <div className="space-y-5">
+                                        <p className="text-[18px] text-[#3e3e3e] dark:text-[#cbcbcb] leading-[40px]">
+                                             با عضویت در این خبرنامه از جدیدترین پست ها با خبر میشی و هیچ چیزی رو از دست نمیدی!
+                                        </p>
+                                        <p className="text-sm">
+                                             استیو جابز
+                                        </p>
+                                   </div>
                               </div>
                          </div>
                     </div>
