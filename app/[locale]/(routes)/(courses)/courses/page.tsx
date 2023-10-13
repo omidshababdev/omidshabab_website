fa067c448fa9c80d1e774e9eb163294b4bad09a9
Omid Shabab <omidshababdev@gmail.com>
2023-10-13 18:07:58 +0100

    first commit

--- app/[locale]/(routes)/(courses)/courses/page.tsx
+++ app/[locale]/(routes)/(courses)/courses/page.tsx
@@ -1,5 +1,6 @@
 import CourseItem from "@/components/course-item"
 import Sidebar from "@/components/sidebar"
+import Courses from "@/data/courses.json"
 
 export default function CoursesPage({
      params: { locale }
@@ -10,9 +11,17 @@ export default function CoursesPage({
           <div className="flex flex-1 w-full min-h-max py-2 font-normal gap-[30px] px-5 sm:px-0">
                <div className="w-full">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px] gap-y-[50px]">
-                         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
-                              <CourseItem key={index} />
-                         ))}
+                         {Courses.map((index) =>
+                              index.lang === locale && (
+                                   <CourseItem
+                                        key={index}
+                                        title={index.title}
+                                        price={index.price}
+                                        category={index.category}
+                                        slug={index.slug}
+                                   />
+
+                              ))}
                     </div>
                </div>
                <Sidebar />
--- components/course-item.tsx
+++ components/course-item.tsx
@@ -2,21 +2,31 @@
 
 import { useRouter } from "next/navigation";
 
-const CourseItem = () => {
+const CourseItem = ({
+     title,
+     price,
+     category,
+     slug,
+}: {
+     title: string,
+     price: string,
+     category: string,
+     slug: string
+}) => {
      const router = useRouter()
 
      return (
 
-          <div onClick={() => router.push("/courses/software-development/nextjs")} className="group space-y-5 cursor-pointer">
+          <div onClick={() => router.push(`/courses/${category}/${slug}`)} className="group space-y-5 cursor-pointer">
                <div className="bg-black bg-opacity-[5%] group-hover:bg-opacity-[8%] dark:bg-white dark:bg-opacity-[5%] dark:group-hover:bg-opacity-[8%] w-full aspect-[6/4]">
 
                </div>
                <div className="space-y-2">
                     <p className="text-lg">
-                         عنوان دوره آموزشی
+                         {title}
                     </p>
                     <p className="text-sm text-[#3e3e3e] dark:text-[#cbcbcb] leading-[28px]">
-                         با عضویت در این خبرنامه از جدیدترین پست ها با خبر میشی و هیچ چیزی رو از دست نمیدی!
+                         {price}
                     </p>
                </div>
           </div>
